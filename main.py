e1b448d89791304e7b5c6ea47cc276ebf12d139d
0xSpai <spairoblox@gmail.com>
2024-08-13 23:21:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import modules.twitch.clip_fetch as clip_fetch
+import time
+import modules.twitch.vod_fetch as vod_fetch
+import modules.twitch.clip_download as clip_download
+clip_downloader = clip_download.ClipsDownloader()
 
-clips = clip_fetch.retrieve_vod_clips("")
-print(clips)
+selected_vod = vod_fetch.get_relevant_vod()
+clips, vod_title = vod_fetch.get_vod_clips(str(selected_vod))
+clip_order = 0
+
+for clip_key, data in clips.items():
+    clip_order += 1
+    clip_downloader.download_clip(data, "vod", vod_title, str(clip_order))
+    
+    time.sleep(1)
+
+print("Finished downloading clips")
